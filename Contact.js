c0e241f92a7f426690853ebd56d189eb96e22e0b
Joel Bradley <bradleydjoe@gmail.com>
2021-08-20 14:36:12 +1000

    Add project files.

---
+++ Contact.js
@@ -0,0 +1,68 @@
+﻿        /**
+         * Resets the colour of the input fields
+        */
+        function clearForm() {
+            document.getElementById("fullName").style.backgroundColor = "#ddd";
+            document.getElementById("email").style.backgroundColor = "#ddd";
+            document.getElementById("phone").style.backgroundColor = "#ddd";
+            document.getElementById("message").style.backgroundColor = "#ddd";
+        }
+        /**
+         * Validates form
+         * Create variables for often used HTML elements
+         * Declare RegExp for email validation
+         * Validate full name
+         * Validate email vs RegExp
+         * Validate phone number
+         * Ensure either email or phone number is valid
+         * Validate message
+         */
+        function validate() {
+            clearForm();
+            var valid = true;
+            var eValid = false;
+            var pValid = false;
+            var n = document.forms["form"]["fName"];
+            var e = document.forms["form"]["em"];
+            var p = document.forms["form"]["ph"];
+            var eReg = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+            if (n.value.length < 2 || !n.value.match(/^[A-Za-z]+$/)) {
+                valid = false;
+                document.getElementById("fullName").style.backgroundColor = "#E33";
+            }
+            if (!e.value.match(eReg)) {
+                valid = false;
+                eValid = false;
+                document.getElementById("email").style.backgroundColor = "#E33";
+            } else {
+                eValid = true;
+                document.getElementById("email").style.backgroundColor = "#ddd";
+            }
+            if (!p.value.match(/^[0-9]+$/)) {
+                valid = false;
+                pValid = false;
+                document.getElementById("phone").style.backgroundColor = "#E33";
+            } else {
+                pValid = true;
+                document.getElementById("phone").style.backgroundColor = "#ddd";
+            }
+            if (pValid == false && eValid == false) {
+                document.getElementById("phone").style.backgroundColor = "#E33";
+                document.getElementById("email").style.backgroundColor = "#E33";
+                valid = false;
+            } else {
+                if (pValid == false) {
+                    document.getElementById("phone").value = "";
+                }
+                if (eValid == false) {
+                    document.getElementById("email").value = "";
+                }
+                document.getElementById("phone").style.backgroundColor = "#ddd";
+                document.getElementById("email").style.backgroundColor = "#ddd";
+            }
+            if (document.forms["form"]["mes"].value == "") {
+                valid = false;
+                document.getElementById("message").style.backgroundColor = "#E33";
+            }
+            return valid;
+        }
---
+++ Pricing.js
@@ -0,0 +1,58 @@
+﻿        var dates = new Array();
+        
+        /** 
+        * Adds selected date to date array
+        */
+        function addDate() {
+            dates.push(document.getElementById("calendar").value);
+            dates.sort();
+            displayDates();
+        }
+        /** 
+        * Removes first found entry of a date in the array and removes it
+        */
+        function removeDate() {
+            var idx = dates.indexOf(document.getElementById("calendar").value);
+            if(idx != -1) {
+                dates.splice(idx, 1);
+                displayDates();
+            }
+        }
+        /** 
+        * Displays a list of all dates found in array with prices
+        */
+        function displayDates() {
+            var total = 0;
+            document.getElementById("out").innerHTML = "Dates:"
+            for(var i = 0; i < dates.length; i++) {
+                document.getElementById("out").innerHTML += "</br>" + dates[i] + " - $" + getPrice(dates[i]);
+                total += getPrice(dates[i]);
+            }
+            document.getElementById("total").innerHTML = "$" + total;
+        }
+        /** 
+        * Gets the price for provided date
+        */
+        function getPrice(date) {
+            date = new Date(date);
+            var winStart = new Date('2021-06-01');
+            var sprStart = new Date('2021-09-01');
+            var sumStart = new Date('2021-12-19');
+            var autStart = new Date('2022-01-31');
+            switch (true) {
+                case (date >= winStart && date < sprStart):
+                    return 200;
+                    break;
+                case (date >= sprStart && date < sumStart):
+                    return 210;
+                    break;
+                case (date >= sumStart && date < autStart):
+                    return 220;
+                    break;
+                case (date >= autStart):
+                    return 250;
+                    break;
+                default:
+                    break;
+            }
+        }
